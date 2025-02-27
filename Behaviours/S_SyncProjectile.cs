b70c1abe57d63e9f9823aaaa8a99914f3c8c893f
XShadeSlayerXx <xshadeslayerxx@gmail.com>
2025-02-27 05:49:30 -0600

    added a sync_projectile component. maybe itll help?

---
+++ Behaviours/S_SyncProjectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class S_SyncProjectile : MonoBehaviour
+{
+    SyncProjectile sync = null;
+    void Start()
+    {
+        sync = GetComponentInParent<SyncProjectile>();
+        sync.active = true;
+    }
+}
