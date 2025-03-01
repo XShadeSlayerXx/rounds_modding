ed068ea2449bae667bf90a77dcf5ee11295c7eec
XShadeSlayerXx <xshadeslayerxx@gmail.com>
2025-03-01 09:05:20 -0600

    buffed stinky

--- Behaviours/Stinky.cs
+++ Behaviours/Stinky.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 using UnboundLib;
 using System.Linq;
 using Shade;
+using UnboundLib.GameModes;
+using System.Collections.Generic;
 
 public class Stinky : PlayerHook
 {
     int maxStacks = 3;
     float currentStacks = 0f;
 
-    float movespeedPerStack = .2f;
+    float movespeedPerStack = .33f;
     float jumpHeightPerStack = .1f;
-    float stackDuration = 6f;
+    //float stackDuration = 6f;
+    List<StatChangeTracker> effects = new List<StatChangeTracker>();
 
     GameObject cloud;
 
@@ -37,44 +40,51 @@ public class Stinky : PlayerHook
         }
     }
 
+    public override IEnumerator OnBattleStart(IGameModeHandler gameModeHandler)
+    {
+        foreach (var effect in effects)
+        {
+            StatManager.Remove(effect);
+        }
+        currentStacks = 0f;
+        return base.OnBattleStart(gameModeHandler);
+    }
+
     public override IEnumerator OnOutOfAmmoCoroutine(int bulletsReloaded)
     {
-        //gain 1 stack per full reload. do not go over maxStacks
-        float stacksToGain = Mathf.Min(bulletsReloaded / gunAmmo.maxAmmo, maxStacks - currentStacks);
-        currentStacks += stacksToGain;
-
-        float movementToGain = 1 + stacksToGain * movespeedPerStack;
-        float jumpToGain = 1 + stacksToGain * jumpHeightPerStack;
-
-        var statsToChange = new StatChanges
+        if (currentStacks < maxStacks)
         {
-            MovementSpeed = movementToGain,
-            JumpHeight = jumpToGain
-        };
+            //gain 1 stack per full reload. do not go over maxStacks
+            float stacksToGain = Mathf.Min(bulletsReloaded / gunAmmo.maxAmmo, maxStacks - currentStacks);
+            currentStacks += stacksToGain;
 
-        var effect = StatManager.Apply(player, statsToChange);
+            float movementToGain = 1 + stacksToGain * movespeedPerStack;
+            float jumpToGain = 1 + stacksToGain * jumpHeightPerStack;
 
-        // create the green cloud
-        var obj = Instantiate(cloud, player.transform, false);
-        obj.transform.position = player.transform.position;
-        obj.GetOrAddComponent<RemoveAfterSeconds>().seconds = stackDuration;
-        ParticleSystem ps = obj.GetComponent<ParticleSystem>();
-        ParticleSystem.MainModule why = ps.main;
-        why.duration = stackDuration * .95f; //end slightly before the stack ends to be a better visual cue
+            var statsToChange = new StatChanges
+            {
+                MovementSpeed = movementToGain,
+                JumpHeight = jumpToGain
+            };
 
-        // play the sound
-        ShadeCards.playSoundFromAsset("balloon", player.transform, .6f);
+            var effect = StatManager.Apply(player, statsToChange);
+            effects.Add(effect);
+            // create the green cloud
+            var obj = Instantiate(cloud, player.transform, false);
+            obj.transform.position = player.transform.position;
+            ParticleSystem ps = obj.GetComponent<ParticleSystem>();
+            ParticleSystem.MainModule why = ps.main;
 
-        yield return new WaitForSeconds(stackDuration);
-        StatManager.Remove(effect);
-        currentStacks -= stacksToGain;
+            // play the sound
+            ShadeCards.playSoundFromAsset("balloon", player.transform, .6f);
 
-        //Destroy(obj);
+            yield return null;
+        }
     }
 
     public void Upgrade()
     {
-        maxStacks += 1;
-        stackDuration *= 1.2f;
+        maxStacks *= 2;
+        //stackDuration *= 1.2f;
     }
 }
